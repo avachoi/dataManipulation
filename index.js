e0e27e46b7e7398bcb6e3f9f2551fd4104f9748d
avachoi <alexbchoi@gmail.com>
2024-05-21 13:32:58 -0400

    finished part2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Part1
 // The initial numbers that must be verified.
 const n1 = 10;
 const n2 = 15;
@@ -12,10 +13,33 @@ const isDivisibleBy5 =
 const isFirstLargerThanLast = n1 > n4;
 // Check three: calculate
 const calculate = ((n2 - n1) * n3) % n4 === 0;
-console.log(calculate);
 const isValid = isDivisibleBy5 && isFirstLargerThanLast && calculate;
 
 // Finally, log the results.
 console.log(
 	`The four numbers are valid according to the provided criteria: ${isValid}`
 );
+
+//Part2
+//The fixed values for the trip
+const distance = 1500;
+const fuelBudget = 175;
+const fuelCost = 3;
+
+//each plan for the trip
+let plan1 = { perHour: 55, perGallon: 30 };
+let plan2 = { perHour: 60, perGallon: 28 };
+let plan3 = { perHour: 75, perGallon: 23 };
+
+//created a function to calculate result with all the factors
+function isEfficient(plan) {
+	let hours = (distance / plan.perHour).toFixed(2);
+	let gallons = (distance / plan.perGallon).toFixed(2);
+	let cost = ((distance / plan.perGallon) * fuelCost).toFixed(2);
+	let isInBudget = cost < fuelBudget ? "in budget" : "out of budget";
+	return `We'll need ${gallons} gallons and it's ${isInBudget}. The trip will take ${hours} hours`;
+}
+
+console.log(isEfficient(plan1));
+console.log(isEfficient(plan2));
+console.log(isEfficient(plan3));
